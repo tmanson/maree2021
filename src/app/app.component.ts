f652ae16667948d098991fecc9be4596d5f86954
tmanson <manson.informatique@gmail.com>
2020-11-29 10:07:50 +0100

    ajout des assets coefficients

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,18 +6,32 @@ import { map } from 'rxjs/operators';
 
 import { EChartOption } from 'echarts';
 
-import janvier from '../assets/janvier.json';
-import fevrier from '../assets/fevrier.json';
-import mars from '../assets/mars.json';
-import avril from '../assets/avril.json';
-import mai from '../assets/mai.json';
-import juin from '../assets/juin.json';
-import juillet from '../assets/juillet.json';
-import aout from '../assets/aout.json';
-import septembre from '../assets/septembre.json';
-import octobre from '../assets/octobre.json';
-import novembre from '../assets/novembre.json';
-import decembre from '../assets/decembre.json';
+import janvier from '../assets/hauteurs/janvier.json';
+import fevrier from '../assets/hauteurs/fevrier.json';
+import mars from '../assets/hauteurs/mars.json';
+import avril from '../assets/hauteurs/avril.json';
+import mai from '../assets/hauteurs/mai.json';
+import juin from '../assets/hauteurs/juin.json';
+import juillet from '../assets/hauteurs/juillet.json';
+import aout from '../assets/hauteurs/aout.json';
+import septembre from '../assets/hauteurs/septembre.json';
+import octobre from '../assets/hauteurs/octobre.json';
+import novembre from '../assets/hauteurs/novembre.json';
+import decembre from '../assets/hauteurs/decembre.json';
+
+import coeffjanvier from '../assets/coeff/coeffJanvier.json';
+import coefffevrier from '../assets/coeff/coeffFevrier.json';
+import coeffmars from '../assets/coeff/coeffMars.json';
+import coeffavril from '../assets/coeff/coeffAvril.json';
+import coeffmai from '../assets/coeff/coeffMai.json';
+import coeffjuin from '../assets/coeff/coeffJuin.json';
+import coeffjuillet from '../assets/coeff/coeffJuillet.json';
+import coeffaout from '../assets/coeff/coeffAout.json';
+import coeffseptembre from '../assets/coeff/coeffSeptembre.json';
+import coeffoctobre from '../assets/coeff/coeffOctobre.json';
+import coeffnovembre from '../assets/coeff/coeffNovembre.json';
+import coeffdecembre from '../assets/coeff/coeffDecembre.json';
+
 import { formatNumber } from '@angular/common';
 
 @Component({
@@ -34,13 +48,31 @@ export class AppComponent implements OnInit {
     },
     yAxis: {
       type: 'value',
-      max: 6
+      max: 100
     },
     series: [
       {
         data: [],
         type: 'line',
+        label: {
+          normal: {
+            show: true,
+            position: 'top'
+          },
+          formatter: null
+        },
       },
+      {
+        data: [],
+        type: 'line',
+        label: {
+          normal: {
+            show: true,
+            position: 'top'
+          },
+          formatter: null
+        },
+      }
     ],
   };
   optionsJanvier: Options = null;
@@ -72,18 +104,18 @@ export class AppComponent implements OnInit {
     this.optionsNovembre = AppComponent.deepCopy(this.options);
     this.optionsDecembre = AppComponent.deepCopy(this.options);
     this.months = [
-      { month: "January", days: 31, monthDatas: janvier, options: this.optionsJanvier },
-      { month: "February", days: 28, monthDatas: fevrier, options: this.optionsFevrier },
-      { month: "March", days: 31, monthDatas: mars, options: this.optionsMars },
-      { month: "April", days: 30, monthDatas: avril, options: this.optionsAvril },
-      { month: "May", days: 31, monthDatas: mai, options: this.optionsMai },
-      { month: "June", days: 30, monthDatas: juin, options: this.optionsJuin },
-      { month: "July", days: 31, monthDatas: juillet, options: this.optionsJuillet },
-      { month: "August", days: 31, monthDatas: aout, options: this.optionsAout },
-      { month: "September", days: 30, monthDatas: septembre, options: this.optionsSeptembre },
-      { month: "October", days: 31, monthDatas: octobre, options: this.optionsOctobre },
-      { month: "November", days: 30, monthDatas: novembre, options: this.optionsNovembre },
-      { month: "December", days: 31, monthDatas: decembre, options: this.optionsDecembre },
+      { month: "January", days: 31, monthDatas: janvier, options: this.optionsJanvier, coeffDatas: coeffjanvier },
+      { month: "February", days: 28, monthDatas: fevrier, options: this.optionsFevrier, coeffDatas: coefffevrier },
+      { month: "March", days: 31, monthDatas: mars, options: this.optionsMars, coeffDatas: coeffmars },
+      { month: "April", days: 30, monthDatas: avril, options: this.optionsAvril, coeffDatas: coeffavril },
+      { month: "May", days: 31, monthDatas: mai, options: this.optionsMai, coeffDatas: coeffmai },
+      { month: "June", days: 30, monthDatas: juin, options: this.optionsJuin, coeffDatas: coeffjuin },
+      { month: "July", days: 31, monthDatas: juillet, options: this.optionsJuillet, coeffDatas: coeffjuillet },
+      { month: "August", days: 31, monthDatas: aout, options: this.optionsAout, coeffDatas: coeffaout },
+      { month: "September", days: 30, monthDatas: septembre, options: this.optionsSeptembre, coeffDatas: coeffseptembre },
+      { month: "October", days: 31, monthDatas: octobre, options: this.optionsOctobre, coeffDatas: coeffoctobre },
+      { month: "November", days: 30, monthDatas: novembre, options: this.optionsNovembre, coeffDatas: coeffnovembre },
+      { month: "December", days: 31, monthDatas: decembre, options: this.optionsDecembre , coeffDatas: coeffdecembre},
     ]
    }
 
@@ -91,6 +123,8 @@ export class AppComponent implements OnInit {
     const OFFSET_HEURE = 0;
     const OFFSET_HAUTEUR = 1;
     let m = 0;
+    const NB_COEFF = 4;
+    const COEFF_SERIE = 1;
     this.months.forEach((month) => {
       console.log("Month", month, m);
       for (let d = 0; d < month.days; d++) {
@@ -101,7 +135,14 @@ export class AppComponent implements OnInit {
           month.options.xAxis.data.push(month.monthDatas[d][date][i][OFFSET_HEURE]);
           month.options.series[0].data.push(month.monthDatas[d][date][i][OFFSET_HAUTEUR]);
         }
+        for (let i = 0; i < NB_COEFF; i++) {
+          let coeff = month.coeffDatas[d][date][i];
+          if(coeff[0] == 'tide.high') {
+            month.options.series[COEFF_SERIE].data.push(coeff[3]);
+          }
+        }
       }
+      month.options.series[0].label.formatter = (params) => { params.value[params.encode.y[0]]};
       m++;
     });
   }
@@ -139,7 +180,7 @@ export class AppComponent implements OnInit {
     throw new Error("Unable to copy obj! Its type isn't supported.");
   }
 }
-interface MonthObj { month: string, days: number, monthDatas: Array<Object>, options: Options }
+interface MonthObj { month: string, days: number, monthDatas: Array<Object>, options: Options, coeffDatas: Object }
 interface Options {
   xAxis: {
     type: string,
@@ -153,6 +194,24 @@ interface Options {
     {
       data: Array<Number>,
       type: string,
+      label: {
+        normal: {
+          show: true,
+          position: 'top'
+        },
+        formatter: any
+      },
     },
+    {
+      data: Array<Number>,
+      type: string,
+      label: {
+        normal: {
+          show: true,
+          position: 'top'
+        },
+        formatter: any
+      },
+    }
   ],
 };
